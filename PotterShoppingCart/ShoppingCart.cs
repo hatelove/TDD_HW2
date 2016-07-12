a655bc78f2198a3dac50861f3d46ef571a2fdc3b
James <spidyjames@gmail.com>
2016-07-13 00:39:04 +0800

    Day2 homework

---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class ShoppingCart
+    {
+        Dictionary<string, int> shoopingCar = new Dictionary<string, int>
+            {
+                { "1", 0}, { "2",0}, { "3", 0}, { "4",0}, {"5",0 }
+            };
+
+
+        public double CalculatePrice(Dictionary<string, int> shoopingCar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void CalculatePriceTest_第一集買一本_價格為_100()
+        {
+            // arrange
+            Dictionary<string, int> orders = new Dictionary<string, int>
+            {
+                { "1", 1}, { "2",0}, { "3", 0}, { "4",0}, {"5",0 }
+            };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            double expected = 100;
+
+            // act
+            double actual = shoppingCart.CalculatePrice(orders);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
