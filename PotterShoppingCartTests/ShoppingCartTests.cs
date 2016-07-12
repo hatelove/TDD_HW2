e904a16f99ffac497ae5d133109067da6064f7f7
James <spidyjames@gmail.com>
2016-07-13 01:32:50 +0800

    day2 homework

--- PotterShoppingCartTests/ShoppingCartTests.cs
+++ PotterShoppingCartTests/ShoppingCartTests.cs
@@ -54,5 +54,94 @@ namespace ShoppingCart.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculatePriceTest_第一二三集各買一本_價格為_270()
+        {
+            // arrange
+            BookInfo bookInfo = new BookInfo();
+
+            bookInfo.Order = new Dictionary<string, int>
+            {
+                { "1", 1}, { "2",1}, { "3", 1}, { "4",0}, {"5",0 }
+            };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            double expected = 270;
+
+            // act
+            double actual = shoppingCart.CalculatePrice(bookInfo);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculatePriceTest_第一二三四集各買一本_價格為_320()
+        {
+            // arrange
+            BookInfo bookInfo = new BookInfo();
+
+            bookInfo.Order = new Dictionary<string, int>
+            {
+                { "1", 1}, { "2",1}, { "3", 1}, { "4",1}, {"5",0 }
+            };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            double expected = 320;
+
+            // act
+            double actual = shoppingCart.CalculatePrice(bookInfo);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculatePriceTest_第一二三四五集買一本_價格為_375()
+        {
+            // arrange
+            BookInfo bookInfo = new BookInfo();
+
+            bookInfo.Order = new Dictionary<string, int>
+            {
+                { "1", 1}, { "2",1}, { "3", 1}, { "4",1}, {"5",1}
+            };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            double expected = 375;
+
+            // act
+            double actual = shoppingCart.CalculatePrice(bookInfo);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculatePriceTest_第一二集各買一本_第三集買兩本_價格為_370()
+        {
+            // arrange
+            BookInfo bookInfo = new BookInfo();
+
+            bookInfo.Order = new Dictionary<string, int>
+            {
+                { "1", 1}, { "2",1}, { "3", 2}, { "4",0}, {"5",0}
+            };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            double expected = 370;
+
+            // act
+            double actual = shoppingCart.CalculatePrice(bookInfo);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
